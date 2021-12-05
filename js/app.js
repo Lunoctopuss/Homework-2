a25bce2352b4b87b004d9b139d452166130605c2
Mona Lisa <krojek7@gmail.com>
2021-12-05 17:17:31 +0000

    commit

--- js/app.js
+++ js/app.js
@@ -3,20 +3,14 @@
 let currentDate;
 let dateDob;
 let dateUtc;
-let day;
 
 currentDate = new Date();
-currentDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(), currentDate.getHours(), currentDate.getMinutes(), currentDate.getSeconds());
 
 dateDob = new Date();
-dateDob.setFullYear(1995);
-dateDob.setMonth(2);
-dateDob.setDate(9);
+dateDob.setFullYear(1995, 02, 09);
 dateDob.setHours(4);
 dateDob.setMinutes(0);
 dateDob.setSeconds(0);
-
-// dateUtc = ( date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
 
 let inputDate0 = alert(`Current date is ${currentDate}`)
 
@@ -24,25 +18,25 @@ let inputDate1 = alert(`My date of birth is ${dateDob}`);
 
 let inputDate2 = alert(`I was born on the ${dateDob.getDate()}th.`)
 
-let inputDate3 = alert(`Current date converted to UTC is ${new Date().toISOString()} and my DOB converted to UTC is ${new Date(dateDob).toISOString()}.`)
+let inputDate3 = alert(`Current date converted to UTC is ${new Date().toUTCString()} and my DOB converted to UTC is ${new Date(dateDob).toUTCString()}.`)
 
 
 // TASK 2
 
-const colorBlue = ['blue'];
+const colours = ['blue', 'pink', 'red', 'yellow', 'green', 'orange', 'purple', 'white', 'black'];
 
 initialPrompt = () => {
-    let ans = confirm('Would you like to change your background color?');
+    let ans = confirm('Would you like to change your background colour?');
     if (ans) {
-        inputColorFunc();
+        inputColourFunc();
     }
 };
 
-inputColorFunc = () => {
-    let varColor = prompt(`Please enter a color blue.`);
-    if (colorBlue.includes(varColor.toLowerCase())) {
-        let changeColor = (document.querySelector('body').style.backgroundColor = `#8ecae6`);
-        alert("You've changed your background color.");
+inputColourFunc = () => {
+    let varColour = prompt(`Please enter a colour.`);
+    if (colours.includes(varColour.toLowerCase())) {
+        document.body.style.backgroundColor = varColour;
+        alert("You've changed your background colour.");
     } else {
         alert("Incorrect data entered.");
         initialPrompt();
@@ -53,6 +47,24 @@ initialPrompt();
 
 // TASK 3
 
+
+function funcImg1() {
+    let image_Id = document.getElementById('changePic1');
+    if (image_Id.src.match("https://images.pexels.com/photos/2150/sky-space-dark-galaxy.jpg?cs=srgb&dl=pexels-pixabay-2150.jpg&fm=jpg")) {
+        image_Id.src = "https://images.pexels.com/photos/10325736/pexels-photo-10325736.jpeg?cs=srgb&dl=pexels-eriks-cistovs-10325736.jpg&fm=jpg";
+    }
+    else {
+        image_Id.src = "https://images.pexels.com/photos/2150/sky-space-dark-galaxy.jpg?cs=srgb&dl=pexels-pixabay-2150.jpg&fm=jpg";
+    }
+}   
+
+funcImg1();
+
+// function changeImg (a)
+
+// document.getElementById("pic1").addEventListener("click", changePic);
+
+// document.getElementById("testID").style.color = "purple";
 
 // let example = document.getElementsByTagName(`h1`)[0].innerHTML;
 // let example = document.querySelector(`div`).style.color = `blue`;
